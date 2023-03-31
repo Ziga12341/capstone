dcad55d602fa3638e82e21cd105b4ee511c5d003
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-03-31 14:28:52 +0200

    added register.html, login.html, layout.html, and start working on index.html page,
    issue with register new user

--- votingapp/views.py
+++ votingapp/views.py
@@ -1,8 +1,82 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from .models import User
 
-
-# Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "votingapp/index.html")
+
+
+def login_view(request):
+    """
+    Log user in.
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "votingapp/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "votingapp/login.html")
+
+
+def logout_view(request):
+    """
+    Log user out.
+    :param request:
+    :return:
+    """
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    """
+    Register user.
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "votingapp/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "votingapp/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "votingapp/register.html")
