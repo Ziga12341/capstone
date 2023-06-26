c70febad64ce7af618a8bd327fc4c137d77686e8
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-06-26 14:31:58 +0200

    suggestion: committed old changes

--- votingapp/urls.py
+++ votingapp/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("group/<str:group_name>/<str:category_name>", views.category, name="category"),
     path("group/<str:group_name>/<str:category_name>/<str:suggestion_name>", views.suggestion, name="suggestion"),
     path("group/<str:group_name>/<str:category_name>/<str:suggestion_name>/vote", views.vote, name="vote"),
+    # path("group/<str:group_name>/<str:category_name>/<str:suggestion_name>/comment", views.comment, name="comment"),
 ]
